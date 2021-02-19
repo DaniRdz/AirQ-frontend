a1cac97ad6f9e259feac8020147c98f47e24cfec
RdzCore <daniel_rod_con@hotmail.com>
2021-02-19 13:54:16 -0600

    created home component and added some styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
 import Navigation from "./navigation";
+import Home from "./home";
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
         <Navigation />
+        <Home />
       </div>
     );
   }
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <div>Content goes here amix</div>
+      </div>
+    );
+  }
+}
