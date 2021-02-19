65906317e15ea5fad4d34f62c80c4abd3238296a
RdzCore <daniel_rod_con@hotmail.com>
2021-02-19 14:32:01 -0600

    added getCurrentPosition via webAPI

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,27 @@ import Navigation from "./navigation";
 import Home from "./home";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      latitude: "",
+      longitude: "",
+    };
+  }
+  getCurrentPosition() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+
+      this.setState({
+        longitude,
+        latitude,
+      });
+    });
+  }
+  componentDidMount() {
+    this.getCurrentPosition();
+  }
   render() {
     return (
       <div className="container">
