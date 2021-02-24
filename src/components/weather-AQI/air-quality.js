68aeef810abb5925737db70c96a9bd4174f049cc
RdzCore <daniel_rod_con@hotmail.com>
2021-02-23 20:29:06 -0600

    ceated getAirQualityMessage

--- src/components/weather-AQI/air-quality.js
+++ src/components/weather-AQI/air-quality.js
@@ -4,6 +4,31 @@ export default class AirQuality extends Component {
   constructor(props) {
     super(props);
   }
+
+  getAirQualityMessage(aqi) {
+    if (typeof aqi === "number") {
+      let status = "";
+      if (aqi < 50) {
+        status = "Good";
+        return status;
+      } else if (aqi > 50 && aqi < 100) {
+        status = "Moderate";
+        return status;
+      } else if (aqi > 100 && aqi < 150) {
+        status = "Unhealthy For Sensitive Groups";
+        return status;
+      } else if (aqi > 150 && aqi < 200) {
+        status = "Unhealthy";
+        return status;
+      } else if (aqi > 200 && aqi < 300) {
+        status = "Very Unhealthy";
+        return status;
+      } else if (aqi > 300 && aqi < 500) {
+        status = "Hazardous";
+        return status;
+      }
+    }
+  }
   render() {
     const { city, state, country, current } = this.props.cityAQI;
     const { pollution } = current;
@@ -26,7 +51,9 @@ export default class AirQuality extends Component {
                   <div className="aqi">{aqicn}</div>
                 </div>
               </div>
-              <div className="air-quality-message">GOOD</div>
+              <div className="air-quality-message">
+                {this.getAirQualityMessage(aqius)}
+              </div>
             </div>
           </div>
         </div>
