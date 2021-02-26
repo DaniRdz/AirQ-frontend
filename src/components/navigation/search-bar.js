e15155713a006fcb83a120c21c1de2a2a43b64dc
RdzCore <daniel_rod_con@hotmail.com>
2021-02-26 15:12:34 -0600

    install leaflet-geosearch package for find cities

--- src/components/navigation/search-bar.js
+++ src/components/navigation/search-bar.js
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
+import { OpenStreetMapProvider } from "leaflet-geosearch";
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      city: "",
+    };
+
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      city: event.target.value,
+    });
+  }
+
+  handleFormSubmit(event) {
+    const provider = new OpenStreetMapProvider();
+
+    provider.search({ query: this.state.city }).then((result) => {
+      console.log(result);
+      this.setState({
+        city: "",
+      });
+    });
+    event.preventDefault();
+  }
   render() {
     return (
-      <div className="search-bar">
-        <input type="text" name="city" placeholder="Search City or Zip Code" />
-      </div>
+      <form onSubmit={this.handleFormSubmit} className="search-bar">
+        <input
+          type="text"
+          name="city"
+          placeholder="Search City or Zip Code"
+          value={this.state.city}
+          onChange={this.handleChange}
+        />
+      </form>
     );
   }
 }
