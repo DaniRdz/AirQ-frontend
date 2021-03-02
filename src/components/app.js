897534546e1556a706f3ba2c7fcb634e977a14c2
RdzCore <daniel_rod_con@hotmail.com>
2021-03-02 13:16:04 -0600

    erase console.log

--- src/components/app.js
+++ src/components/app.js
@@ -32,7 +32,6 @@ export default class App extends Component {
         return city.city !== deletedCity.city;
       }),
     });
-    console.log("deleted", city);
   }
   getNewCityData(city) {
     this.setState({
